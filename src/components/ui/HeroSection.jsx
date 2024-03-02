64e0a83f4f2c6089d98a7ac212a0f620d98c8aac
Afsana Meem <afsumeem@gmail.com>
2024-03-02 21:26:31 +0600

    hero section done

---
+++ src/components/ui/HeroSection.jsx
@@ -0,0 +1,32 @@
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import headerImage from "../../assets/images/headerimage.png";
+
+const HeroSection = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Take ideas from better to best</h1>
+          <p>
+            Miro is your team's visual platform to connect, collaborate, and
+            create — together.
+          </p>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Enter your work email</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Button>Sign up free</Button>
+          </Form>
+          <p>Collaborate with your team within minutes</p>
+          <div></div>
+        </Col>
+        <Col>
+          <img src={headerImage} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default HeroSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import { Container } from "react-bootstrap";
 import Header from "../components/shared/Header";
+import HeroSection from "../components/ui/HeroSection";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <Container>
+        <HeroSection />
+      </Container>
     </div>
   );
 };
